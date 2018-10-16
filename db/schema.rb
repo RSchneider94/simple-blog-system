c2b7e15ceaa036464ca6c9d03b11f9c26b1c19d2
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-10-16 18:46:33 +0200

    Implement Devise configuration, model and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_150306) do
+ActiveRecord::Schema.define(version: 2018_10_16_164010) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,20 @@ ActiveRecord::Schema.define(version: 2018_10_15_150306) do
     t.datetime "updated_at", null: false
     t.integer "category_id"
     t.index ["category_id"], name: "index_posts_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end

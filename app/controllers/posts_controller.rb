1883dafb165f3eac472d334522ee4990cf9ce2a4
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-10-16 17:56:12 +0200

    Refactor PostController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.paginate(page: params[:page])
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -22,11 +22,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: "The post was successfully updated!"
     else
@@ -34,9 +32,15 @@ class PostsController < ApplicationController
     end
   end
 
-  def destroy; end #todo
+  def destroy
+
+  end
 
   private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:author, :title, :category_id, :content)
   end
